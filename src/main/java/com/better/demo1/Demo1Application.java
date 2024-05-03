37622a2fc913b499d6f48d23f44c6a972ca5354d
BETTER <nowqwer@mail.ru>
2024-05-03 19:47:08 +0500

    lesson1

---
+++ src/main/java/com/better/demo1/Demo1Application.java
@@ -0,0 +1,13 @@
+package com.better.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://spring.io/guides/gs/serving-web-content#initial
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
